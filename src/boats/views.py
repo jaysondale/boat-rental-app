a4b4be1290546f953c8d16ce8e1165e12d6fffe5
Jayson Dale <jaysondale1@gmail.com>
2021-03-19 12:12:02 -0400

    Added product list

--- src/boats/views.py
+++ src/boats/views.py
@@ -3,9 +3,9 @@ from django.shortcuts import render
 from .models import Boat
 # Create your views here.
 def boat_detail_view(request):
-	obj = Boat.objects.get(id=1)
+	boats = Boat.objects.all()
 	context = {
 		'page_title': 'KLM Boat Rentals',
-		'object': obj
+		'boats': boats
 	}
 	return render(request, "boats/main.html", context)
