edbb643004c86ab2bce9cfa593b2f491c21cd10d
amavroud <and415@gmail.com>
2021-04-22 12:55:33 -0400

    boat widget

--- src/pages/static/pages/js/main.js
+++ src/pages/static/pages/js/main.js
@@ -1,1 +1,9 @@
-console.log("working");
+$(".boat-image-wrapper").addClass('col-12');
+$(".boat-detail-wrapper").hide();
+
+
+$("#boat-button").click(() => {
+	$(".boat-image-wrapper").removeClass('col-12');
+	$(".boat-image-wrapper").addClass('col-6');
+	$(".boat-detail-wrapper").show();
+})
