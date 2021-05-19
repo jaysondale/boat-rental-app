88d728b17f3cded9c0446694a603d7622375e4cf
Jayson Dale <jaysondale1@gmail.com>
2021-05-19 09:06:04 -0400

    removed auth.User for boats

--- boats/models.py
+++ boats/models.py
@@ -2,7 +2,7 @@ from django.db import models
 from django.forms import ModelForm
 from django.conf import settings
 
-AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL', 'auth.User')
+AUTH_USER_MODEL = getattr(settings, 'AUTH_USER_MODEL')
 
 # Create your models here.
 
