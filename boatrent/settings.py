139e1105f3a02b2dcc7d30988b19dd2ea0859078
Jayson Dale <jaysondale1@gmail.com>
2021-05-26 09:05:18 -0400

    added aws region

--- boatrent/settings.py
+++ boatrent/settings.py
@@ -204,6 +204,9 @@ AWS_ACCESS_KEY_ID = os.environ.get('AWS_ACCESS_KEY_ID')
 AWS_SECRET_ACCESS_KEY = os.environ.get('AWS_SECRET_ACCESS_KEY')
 AWS_STORAGE_BUCKET_NAME = os.environ.get('S3_BUCKET_NAME')
 
+AWS_S3_REGION_NAME = 'ca-central-1'
+AWS_S3_SIGNATURE_VERSION = 's3v4'
+
 STATICFILES_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 DEFAULT_FILE_STORAGE = 'storages.backends.s3boto3.S3Boto3Storage'
 
